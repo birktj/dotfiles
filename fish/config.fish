2e85aa1cddf05d18b829913215f1eb72e97bf1da
Birk Tjelmeland <birktjelmeland@yahoo.no>
2020-03-26 16:02:20 +0100

    Remove ghci alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 set SHELL /bin/fish
 
 alias ssh "env TERM=screen-256color ssh"
-alias ghci /usr/share/ghc-pristine/bin/ghci
+#alias ghci /usr/share/ghc-pristine/bin/ghci
 alias runhaskell /usr/share/ghc-pristine/bin/runhaskell
